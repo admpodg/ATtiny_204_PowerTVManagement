25c8d5d54d3d57c8ae29d4ebdab76a40148fd209
DESKTOP-NI7VK7O\admpo <admpodg@gmail.com>
2022-04-09 00:33:01 +0200

    Init project setup - first commit

---
+++ ATtiny_204_PowerTVManagement/main.c
@@ -0,0 +1,18 @@
+/*
+ * ATtiny_204_PowerTVManagement.c
+ *
+ * Created: 09.04.2022 00:31:40
+ * Author : admpo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
